d84516f9f944594c561a658654fb4922dd04ff27
Palskie <zaisantillan14@gmail.com>
2022-01-12 06:55:03 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles/app.scss";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="name">your friendly neighborhood youka</h1>
+    </div>
+  );
+};
+
+export default App;
